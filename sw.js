7325ae3d3d1caef4457443d5dd7fe60a82937cdb
syake66 <264705713+syake66@users.noreply.github.com>
2026-05-13 17:00:27 +0900

    Refine descriptions and meta tags to be more inclusive and positive

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    バックグラウンド通知・オフライン対応を担う
    ===================================================== */
 
-const CACHE_NAME = 'focusflow-v41';
+const CACHE_NAME = 'focusflow-v42';
 // キャッシュするアセット一覧
 const ASSETS_TO_CACHE = [
   '/',
