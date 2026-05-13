29ea4102cd3f51b472df4d53151b6a47d5622ecc
syake66 <264705713+syake66@users.noreply.github.com>
2026-05-13 13:34:16 +0900

    Refine time display color and update memo icon to solid rounded bubble

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    バックグラウンド通知・オフライン対応を担う
    ===================================================== */
 
-const CACHE_NAME = 'focusflow-v33';
+const CACHE_NAME = 'focusflow-v34';
 // キャッシュするアセット一覧
 const ASSETS_TO_CACHE = [
   '/',
