efa3332434467e9f71c11b52a09638f61edd5456
syake66 <264705713+syake66@users.noreply.github.com>
2026-05-13 13:44:50 +0900

    Restore square checkboxes, style meta time to purple bold, and fix time saving bug

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    バックグラウンド通知・オフライン対応を担う
    ===================================================== */
 
-const CACHE_NAME = 'focusflow-v35';
+const CACHE_NAME = 'focusflow-v36';
 // キャッシュするアセット一覧
 const ASSETS_TO_CACHE = [
   '/',
