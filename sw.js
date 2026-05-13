03169d9da0fbadc62fbcaaa1cb7b66a4525fa457
syake66 <264705713+syake66@users.noreply.github.com>
2026-05-13 10:23:32 +0900

    Revert UI to previous state except for trash and history icons

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    バックグラウンド通知・オフライン対応を担う
    ===================================================== */
 
-const CACHE_NAME = 'focusflow-v24';
+const CACHE_NAME = 'focusflow-v25';
 // キャッシュするアセット一覧
 const ASSETS_TO_CACHE = [
   '/',
