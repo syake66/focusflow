cf339559a3593c03fb9ec15bd4f37e537a43fabb
syake66 <264705713+syake66@users.noreply.github.com>
2026-05-14 20:43:31 +0900

    Apply Google site verification meta tag

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    バックグラウンド通知・オフライン対応を担う
    ===================================================== */
 
-const CACHE_NAME = 'focusflow-v51';
+const CACHE_NAME = 'focusflow-v52';
 // キャッシュするアセット一覧
 const ASSETS_TO_CACHE = [
   '/',
