39caa3eacaad915c50dfecfe3f4a4c8935243e99
syake66 <264705713+syake66@users.noreply.github.com>
2026-05-14 20:45:26 +0900

    Convert legal links to static format for Google crawler

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    バックグラウンド通知・オフライン対応を担う
    ===================================================== */
 
-const CACHE_NAME = 'focusflow-v52';
+const CACHE_NAME = 'focusflow-v53';
 // キャッシュするアセット一覧
 const ASSETS_TO_CACHE = [
   '/',
